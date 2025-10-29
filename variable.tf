d9007d73387af1019e9df77324e03f3103096bde
Rajkumar <rajkumarmanda@outlook.com>
2025-10-29 14:33:18 +0530

    commit

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "sg_name" {
+    default = catalogue
+}
+
+variable "sg_description" {
+    default = "security group allowing all ports"
+}
+
+variable "project_name" {
+    default = roboshop
+}
+
+variable "environment" {
+    default = dev
+}
+
+variable "sg_tags" {
+    default = {}
+}
